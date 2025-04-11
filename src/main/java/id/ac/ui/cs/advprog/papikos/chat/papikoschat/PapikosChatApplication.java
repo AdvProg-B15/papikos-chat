1cc2e156c108eee0d7ae7e3437c21e85cc3a3258
Bakuwan <f4dhliraihan@gmail.com>
2025-04-11 19:35:44 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/papikos/chat/papikoschat/PapikosChatApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.papikos.chat.papikoschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapikosChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapikosChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/chat/papikoschat/PapikosChatApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.papikos.chat.papikoschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapikosChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
