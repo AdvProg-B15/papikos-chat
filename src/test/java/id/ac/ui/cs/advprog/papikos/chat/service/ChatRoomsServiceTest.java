6bfe3034f53f382d888e3e2bf321948a1bf32b3b
Bakuwan <f4dhliraihan@gmail.com>
2025-05-27 14:40:37 +0700

    refactor: changed test name

--- src/test/java/id/ac/ui/cs/advprog/papikos/chat/service/ChatRoomsServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/papikos/chat/service/ChatRoomsServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class ChatRoomServiceImplTest {
+class ChatRoomsServiceTest {
 
     @Mock
     private ChatRoomRepository chatRoomRepository;
